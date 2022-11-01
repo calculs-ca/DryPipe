03e2c07a462d8363eed6bf564201545a7d5803cc
Maxime Lévesque <maxime.levesque@gmail.com>
2022-11-01 12:53:33 -0400

    cleanup deps

--- setup.py
+++ setup.py
@@ -8,24 +8,16 @@ minimal_requirements = [
     'python-socketio==5.6.0',
     'rich==12.6.0',
     'requests==2.27.1',
-    'sshkeyboard==2.3.1'
-]
-
-extra_requirements = [
+    'readchar==4.0.3',
     'parallel-ssh==2.12.0'
 ]
 
-is_full_req = os.environ.get("dry_pipe_is_full_req") == "True"
-
-if is_full_req:
-    minimal_requirements = minimal_requirements + extra_requirements
 
 setup(
     name='dry_pipe',
-    version='0.4.0',
+    version='0.4.1',
     packages=['dry_pipe'],
     install_requires=minimal_requirements,
-    extras_require={"full": extra_requirements},
     include_package_data=True,
     entry_points='''
         [console_scripts]
