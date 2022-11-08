57cefaa66189a5c13b50eb24112d54200cc2ddc4
Maxime Lévesque <maxime.levesque@gmail.com>
2022-11-08 11:22:12 -0500

    0.5.0 python_calls now have their int and float input args parsed

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ minimal_requirements = [
 
 setup(
     name='dry_pipe',
-    version='0.4.2',
+    version='0.5.0',
     packages=['dry_pipe'],
     install_requires=minimal_requirements,
     include_package_data=True,
