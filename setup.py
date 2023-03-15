98076117632672074930ece44595fa20cc293fe3
Maxime Lévesque <maxime.levesque@gmail.com>
2023-03-15 16:29:56 -0400

    0.7.48

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ minimal_requirements = [
 
 setup(
     name='dry_pipe',
-    version='0.7.47',
+    version='0.7.48',
     packages=['dry_pipe'],
     install_requires=minimal_requirements,
     include_package_data=True,
