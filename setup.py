06f9003ea54a93308b27fc1024d917667fa7889b
Maxime Lévesque <maxime.levesque@gmail.com>
2023-03-15 19:04:58 -0400

    0.7.52

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ minimal_requirements = [
 
 setup(
     name='dry_pipe',
-    version='0.7.51',
+    version='0.7.52',
     packages=['dry_pipe'],
     install_requires=minimal_requirements,
     include_package_data=True,
