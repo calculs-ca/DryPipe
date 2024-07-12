9aba2265288f8f34fa9f865169106d42dadd96be
Maxime <maxime.levesque@gmail.com>
2024-07-12 08:57:30 -0400

    no more deps

--- setup.py
+++ setup.py
@@ -1,19 +1,11 @@
 from setuptools import setup
 
-minimal_requirements = [
-    'click==8.1.3',
-    'rich==12.6.0',
-    'requests==2.27.1',
-    'readchar==4.0.3',
-    'parallel-ssh==2.12.0'
-]
 
 
 setup(
     name='dry_pipe',
     version='0.8.0',
     packages=['dry_pipe'],
-    #install_requires=minimal_requirements,
     include_package_data=True,
     entry_points='''
         [console_scripts]
